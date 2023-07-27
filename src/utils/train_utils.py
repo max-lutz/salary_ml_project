6d067d8c0e0d21958102523e0f66c7e0e88b62b3
max_lutz <max.lutz.09@gmail.com>
2023-07-27 13:34:54 -0600

    refactor code and create utils

---
+++ src/utils/train_utils.py
@@ -0,0 +1,216 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+# import dependencies
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVR
+from lightgbm import LGBMRegressor
+from xgboost.sklearn import XGBRegressor
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.linear_model import ElasticNet, BayesianRidge, Ridge, PoissonRegressor, GammaRegressor
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.base import TransformerMixin
+
+from sklearn.model_selection import KFold, train_test_split, cross_validate
+
+from hyperopt import hp
+from hyperopt.pyll.base import scope
+import mlflow
+
+
+def get_hyperparameters_list():
+
+    return [
+        {
+            'type': 'Ridge',
+            'solver': hp.choice('ridge_solver', ['auto', 'svd', 'cholesky', 'lsqr', 'sparse_cg', 'sag', 'saga']),
+            'alpha': hp.quniform('ridge_alpha', 0, 5, 0.01),
+            'vectorizer_max_features': scope.int(hp.quniform('Ridge_vectorizer_max_features', 100, 10_000, 100)),
+        },
+
+        {
+            'type': 'HuberRegressor',
+            'epsilon': hp.uniform('HuberRegressor_epsilon', 1, 5),
+            'max_iter': scope.int(hp.quniform('HuberRegressor_max_iter', 1000, 20_000, 1000)),
+            'alpha': hp.loguniform('HuberRegressor_alpha', -20, 0),
+            'vectorizer_max_features': scope.int(hp.quniform('HuberRegressor_vectorizer_max_features', 100, 10_000, 100)),
+        },
+
+        {
+            'type': 'PoissonRegressor',
+            'alpha': hp.quniform('PoissonRegressor_alpha', 0, 5, 0.01),
+            'max_iter': scope.int(hp.quniform('PoissonRegressor_max_iter', 1000, 20_000, 1000)),
+            'vectorizer_max_features': scope.int(hp.quniform('PoissonRegressor_vectorizer_max_features', 100, 10_000, 100)),
+        },
+
+        {
+            'type': 'GammaRegressor',
+            'alpha': hp.quniform('GammaRegressor_alpha', 0, 5, 0.01),
+            'max_iter': scope.int(hp.quniform('GammaRegressor_max_iter', 1000, 20_000, 1000)),
+            'vectorizer_max_features': scope.int(hp.quniform('GammaRegressor_vectorizer_max_features', 100, 10_000, 100)),
+        },
+
+        {
+            'type': 'SVR',
+            'kernel': hp.choice('SVR_kernel', ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed']),
+            'C': hp.quniform('SVR_C', 0, 5, 0.01),
+            'epsilon': hp.quniform('SVR_epsilon', 0, 5, 0.01),
+            'vectorizer_max_features': scope.int(hp.quniform('SVR_vectorizer_max_features', 100, 10_000, 100)),
+        },
+
+        {
+            'type': 'LGBMRegressor',
+            'learning_rate':    hp.uniform('LGBMRegressor_learning_rate', 0.0001, 0.25),
+            'max_depth':        scope.int(hp.quniform('LGBMRegressor_max_depth', 2, 200, 1)),
+            'colsample_bytree': hp.uniform('LGBMRegressor_colsample_bytree', 0.4, 1),
+            'subsample':        hp.uniform('LGBMRegressor_subsample', 0.6, 1),
+            'num_leaves':       scope.int(hp.quniform('LGBMRegressor_num_leaves', 1, 200, 1)),
+            'min_split_gain':   hp.uniform('LGBMRegressor_min_split_gain', 0, 1),
+            'reg_alpha':        hp.uniform('LGBMRegressor_reg_alpha', 0, 1),
+            'reg_lambda':       hp.uniform('LGBMRegressor_reg_lambda', 0, 1),
+            'n_estimators':     scope.int(hp.quniform('LGBMRegressor_n_estimators', 10, 500, 10)),
+            'vectorizer_max_features': scope.int(hp.quniform('LGBMRegressor_vectorizer_max_features', 100, 10_000, 100)),
+        },
+
+        {
+            'type': 'ElasticNet',
+            'alpha': hp.quniform('ElasticNet_alpha', 0, 5, 0.01),
+            'l1_ratio': hp.quniform('ElasticNet_l1_ratio', 0, 1, 0.01),
+            'max_iter': scope.int(hp.quniform('ElasticNet_max_iter', 1000, 20_000, 1000)),
+            'vectorizer_max_features': scope.int(hp.quniform('ElasticNet_vectorizer_max_features', 100, 10_000, 100)),
+        },
+
+        {
+            'type': 'BayesianRidge',
+            'max_iter': scope.int(hp.quniform('BayesianRidgemax_iter', 1000, 20_000, 1000)),
+            'alpha_1': hp.loguniform('BayesianRidgea_lpha_1', -20, 0),
+            'alpha_2': hp.loguniform('BayesianRidgea_lpha_2', -20, 0),
+            'lambda_1': hp.loguniform('BayesianRidge_lambda_1', -20, 0),
+            'lambda_2': hp.loguniform('BayesianRidge_lambda_2', -20, 0),
+            'vectorizer_max_features': scope.int(hp.quniform('BayesianRidge_vectorizer_max_features', 100, 10_000, 100)),
+        },
+
+        {
+            'type': 'XGBRegressor',
+            'max_depth': scope.int(hp.quniform('XGBRegressor_max_depth', 3, 15, 1)),
+            'n_estimators': scope.int(hp.quniform('XGBRegressor_n_estimators', 10, 500, 10)),
+            'colsample_bytree': hp.uniform('XGBRegressor_colsample_bytree', 0.5, 1.0),
+            'min_child_weight': scope.int(hp.quniform('XGBRegressor_min_child_weight', 0, 10, 1)),
+            'subsample': hp.uniform('XGBRegressor_subsample', 0.5, 1.0),
+            'learning_rate': hp.uniform('XGBRegressor_learning_rate', 0.0001, 0.25),
+            'gamma': hp.uniform('XGBRegressor_gamma', 0, 1),
+            'reg_alpha': hp.quniform('XGBRegressor_reg_alpha', 0, 10, 0.1),
+            'reg_lambda': hp.quniform('XGBRegressor_reg_lambda', 0, 20, 0.1),
+            'objective': 'reg:squarederror',
+            'eval_metric': 'rmse',
+            'vectorizer_max_features': scope.int(hp.quniform('XGBRegressor_vectorizer_max_features', 100, 10_000, 100)),
+        },
+
+        {
+            'type': 'KernelRidge',
+            'alpha': hp.quniform('KernelRidge_alpha', 0, 5, 0.01),
+            'vectorizer_max_features': scope.int(hp.quniform('KernelRidge_vectorizer_max_features', 100, 10_000, 100)),
+        },
+    ]
+
+
+class DenseTransformer(TransformerMixin):
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+    def transform(self, X, y=None, **fit_params):
+        return X.toarray()
+
+
+def get_model_from_str(model_name, params):
+    if model_name == 'Ridge':
+        model = Ridge(**params)
+    elif model_name == 'PoissonRegressor':
+        model = PoissonRegressor(**params)
+    elif model_name == 'GammaRegressor':
+        model = GammaRegressor(**params)
+    elif model_name == 'SVR':
+        model = SVR(**params)
+    elif model_name == 'LGBMRegressor':
+        model = LGBMRegressor(**params, verbosity=-1, force_row_wise=True)
+    elif model_name == 'ElasticNet':
+        model = ElasticNet(**params)
+    elif model_name == 'BayesianRidge':
+        model = BayesianRidge(**params)
+    elif model_name == 'XGBRegressor':
+        model = XGBRegressor(**params)
+    elif model_name == 'KernelRidge':
+        model = KernelRidge(**params)
+    else:
+        return 0
+    return model
+
+
+def load_data(input_path):
+    df = pd.read_csv(input_path)
+    train, test = train_test_split(df, test_size=0.3, stratify=df['experience'], random_state=0)
+    test, val = train_test_split(test, test_size=0.5, stratify=test['experience'], random_state=0)
+
+    X_train, y_train = train.drop(columns=['id', 'target']), train['target'].to_numpy()
+    X_val, y_val = val.drop(columns=['id', 'target']), val['target'].to_numpy()
+    X_test, y_test = test.drop(columns=['id', 'target']), test['target'].to_numpy()
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+
+def generate_preprocessing_pipeline(max_features=1000, cat_cols=['location', 'experience'], text_col_1='title', text_col_2='description'):
+    preprocessing = make_column_transformer(
+        (OneHotEncoder(handle_unknown="infrequent_if_exist", min_frequency=10, sparse_output=False), cat_cols),
+        (TfidfVectorizer(strip_accents='ascii', stop_words='english', max_features=max_features), text_col_1),
+        (TfidfVectorizer(strip_accents='ascii', stop_words='english', max_features=max_features), text_col_2)
+    )
+    return preprocessing
+
+
+def get_parameters_cross_validation():
+    models_list = {
+        'Linear regressor': LinearRegression(),
+        'Ridge': Ridge(),
+        'PoissonRegressor': PoissonRegressor(max_iter=10_000),
+        'GammaRegressor': GammaRegressor(max_iter=1000),
+        'SVR': SVR(),
+        'LGBMRegressor': LGBMRegressor(verbosity=-1, force_row_wise=True),
+        'ElasticNet': ElasticNet(),
+        'BayesianRidge': BayesianRidge(),
+        'XGBRegressor': XGBRegressor(),
+        'KernelRidge': KernelRidge()
+    }
+    scoring = {'neg_mean_squared_error': 'neg_mean_squared_error'}
+    folds = KFold(n_splits=5, shuffle=True, random_state=0)
+
+    return models_list, scoring, folds
+
+
+def select_base_models(X, y):
+    preprocessing = generate_preprocessing_pipeline()
+    models_list, scoring, folds = get_parameters_cross_validation()
+
+    model_perf_matrix = []
+    columns = ['Model', 'Median fit time', 'Mean error']
+    for model_name, model in tqdm(models_list.items()):
+        pipeline = Pipeline([
+            ('preprocessing', preprocessing),
+            ('to_dense', DenseTransformer()),
+            ('model', model)
+        ])
+
+        cv_score = cross_validate(pipeline, X, y, cv=folds,
+                                  scoring=scoring, verbose=0, error_score="raise")
+        model_perf_matrix.append([model_name, round(cv_score['fit_time'].mean(), 3),
+                                  round(np.sqrt(-cv_score['test_neg_mean_squared_error']).mean(), 4)])
+
+        pipeline.fit(X, y)
+
+    df_model_perf = pd.DataFrame(model_perf_matrix, columns=columns)
+    models_preselected = df_model_perf[(df_model_perf['Mean error'] < df_model_perf['Mean error'].min()*1.2) &
+                                       (df_model_perf['Median fit time'] < 1)].Model.to_list()
+    return models_preselected

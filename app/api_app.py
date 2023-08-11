87bc36765531ffd696d998c7d181463821e8e324
max_lutz <max.lutz.09@gmail.com>
2023-08-11 08:10:47 +0200

    create fast api for online predictions

---
+++ app/api_app.py
@@ -0,0 +1,51 @@
+from mangum import Mangum
+import uvicorn
+from fastapi.responses import JSONResponse
+from fastapi import FastAPI
+import os
+import json
+import pickle
+import requests
+import pandas as pd
+
+import nltk
+nltk.download('words')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+nltk.download('vader_lexicon')
+
+
+app = FastAPI()
+handler = Mangum(app)
+
+# Load the saved model
+pipeline_path = "models/model.pkl"
+with open(pipeline_path, 'rb') as file:
+    pipeline = pickle.load(file)
+
+if not os.path.exists('nltkdata'):
+    os.mkdir('nltkdata')
+    nltk.download('wordnet', download_dir='nltkdata')
+    nltk.download('stopwords', download_dir='nltkdata')
+    nltk.download('omw-1.4', download_dir='nltkdata')
+
+nltk.data.path.append("nltkdata")
+
+
+def predict(data):
+
+    data = json.load(data)
+    df = pd.DataFrame([data['title'], data['location'], data['experience'], data['description']],
+                      columns=["title", "location", "experience", "description"],)
+    prediction = pipeline.predict(df)
+    return prediction
+
+
+@app.get('/')
+def my_function(data: str):
+    pred = predict(data)
+    return JSONResponse({"predictions": pred})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9000)

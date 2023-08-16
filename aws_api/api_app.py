4d7bd3ceb6c0ec2e3c00212ab1d18ad445620c87
max_lutz <max.lutz.09@gmail.com>
2023-08-16 19:46:14 +0200

    fix issue with aws api

--- aws_api/api_app.py
+++ aws_api/api_app.py
@@ -11,34 +11,29 @@ from mangum import Mangum
 import uvicorn
 from fastapi.responses import JSONResponse
 from fastapi import FastAPI
-import os
 import json
 import pickle
-import requests
 import pandas as pd
 
 import nltk
-nltk.download('words')
-nltk.download('wordnet')
-nltk.download('omw-1.4')
-nltk.download('vader_lexicon')
+nltk.data.path.append("nltkdata")
 
 
 app = FastAPI()
 handler = Mangum(app)
 
 # Load the saved model
-pipeline_path = "../models/model.pkl"
+pipeline_path = "models/model.pkl"
 with open(pipeline_path, 'rb') as file:
     pipeline = pickle.load(file)
 
-if not os.path.exists('nltkdata'):
-    os.mkdir('nltkdata')
-    nltk.download('wordnet', download_dir='nltkdata')
-    nltk.download('stopwords', download_dir='nltkdata')
-    nltk.download('omw-1.4', download_dir='nltkdata')
+# if not os.path.exists('nltkdata'):
+#     os.mkdir('nltkdata')
+#     nltk.download('wordnet', download_dir='nltkdata')
+#     nltk.download('stopwords', download_dir='nltkdata')
+#     nltk.download('omw-1.4', download_dir='nltkdata')
 
-nltk.data.path.append("nltkdata")
+# nltk.data.path.append("nltkdata")
 
 
 def predict_from_data(data):

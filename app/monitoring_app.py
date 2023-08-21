83a4aeaf06e5be0ba1bd1c400f3e6973878eba66
max_lutz <max.lutz.09@gmail.com>
2023-08-21 17:51:30 -0600

    reload charts at each prediction

--- app/monitoring_app.py
+++ app/monitoring_app.py
@@ -54,56 +54,62 @@ with title_2:
             """)
         st.write("")
 
-
-st.sidebar.header('Test AWS API')
-run_api_test = False
-run_api_test = st.sidebar.button('Run aws api test')
+    st.header('Test AWS API')
+    run_api_test = False
+    run_api_test = st.button('Run aws api test')
 if (run_api_test):
-    df_test = pd.read_csv("data/test.zip").sample(3)
-    api_latency = []
+    placeholder = st.empty()
 
-    for dataset in [df_test]:
-        predictions = []
-        for i in tqdm(range(len(dataset))):
-            start_time = time.time()
-            predictions.append(int(predict(dataset.iloc[i, 1:-1].values.tolist())['predictions'][0]/1000)*1000)
-            api_latency.append(time.time()-start_time)
-        dataset['prediction'] = predictions
-        dataset['latency'] = api_latency
-        dataset['rmse'] = abs(dataset['prediction'] - dataset['target'])
-        dataset['id_plot'] = [int(i) for i in range(len(dataset))]
+    df_test = pd.read_csv("data/test.zip").sample(15)
+    df_test['prediction'] = 0
+    df_test['latency'] = 0
+    df_test['rmse'] = 0
+    df_test['id_plot'] = [int(i) for i in range(len(df_test))]
 
-    st.header("Monitoring dashboard")
-    st.subheader("Latency")
-    col1, col2, col3 = st.columns(3)
-    with (col1):
-        st.markdown(f"##### Request sent: {len(df_test)}")
-    with (col2):
-        st.markdown(f"##### Average latency: {round(df_test['latency'].mean(), 2)} seconds")
-    with (col3):
-        st.markdown(f"##### Latency [99 percentile] sent: {round(df_test['latency'].quantile(0.99), 2)} seconds")
+    for i in tqdm(range(len(df_test))):
+        start_time = time.time()
+        df_test.loc[df_test['id_plot'] == i, 'prediction'] = int(
+            predict(df_test.iloc[i, 1:-5].values.tolist())['predictions'][0]/1000)*1000
+        df_test.loc[df_test['id_plot'] == i, 'latency'] = time.time()-start_time
+        df_test.loc[df_test['id_plot'] == i, 'rmse'] = abs(
+            df_test.iloc[i, :]['prediction'] - df_test.iloc[i, :]['target'])
 
-    fig = px.line(df_test, x="id_plot", y="latency", title='Latency in the last predictions')
-    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+        df_to_display = df_test[df_test['latency'] > 0]
 
-    st.subheader("Predictions errors")
-    col1_1, col2_1, col3_1 = st.columns(3)
-    with (col1_1):
-        st.markdown(f"##### Average salary predicted: ${round(df_test['prediction'].mean(), 0)}")
-    with (col2_1):
-        st.markdown(f"##### Average RMSE: {round(df_test['rmse'].mean(), 2)}")
-    with (col3_1):
-        st.markdown(f"##### RMSE [99 percentile]: {round(df_test['rmse'].quantile(0.99), 0)}")
+        with placeholder.container():
+            st.header("Monitoring dashboard")
+            st.subheader("Latency")
+            col1, col2, col3 = st.columns(3)
+            with (col1):
+                st.markdown(f"##### Request sent: {len(df_to_display)}")
+            with (col2):
+                st.markdown(f"##### Average latency: {round(df_to_display['latency'].mean(), 2)} seconds")
+            with (col3):
+                st.markdown(
+                    f"##### Latency [99 percentile] sent: {round(df_to_display['latency'].quantile(0.99), 2)} seconds")
 
-    df = pd.melt(df_test, id_vars=['id_plot'], value_vars=['prediction', 'target'])
-    fig = px.line(df, x="id_plot", y="value", color="variable", symbol="variable", title='True value vs prediction')
-    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-    fig = px.line(df_test, x="id_plot", y="rmse", title='RMSE')
-    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+            fig = px.line(df_to_display, x="id_plot", y="latency", title='Latency in the last predictions')
+            st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 
-    st.subheader('Last predictions made')
-    st.write(df_test)
+            st.subheader("Predictions errors")
+            col1_1, col2_1, col3_1 = st.columns(3)
+            with (col1_1):
+                st.markdown(f"##### Average salary predicted: ${round(df_to_display['prediction'].mean(), 0)}")
+            with (col2_1):
+                st.markdown(f"##### Average RMSE: {round(df_to_display['rmse'].mean(), 2)}")
+            with (col3_1):
+                st.markdown(f"##### RMSE [99 percentile]: {round(df_to_display['rmse'].quantile(0.99), 0)}")
 
+            df = pd.melt(df_to_display, id_vars=['id_plot'], value_vars=['prediction', 'target'])
+            fig = px.line(df, x="id_plot", y="value", color="variable",
+                          symbol="variable", title='True value vs prediction')
+            st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+            fig = px.line(df_to_display, x="id_plot", y="rmse", title='RMSE')
+            st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+            st.subheader('Last predictions made')
+            st.write(df_to_display)
+            time.sleep(1)
 
 st.sidebar.header('User input')
 title = st.sidebar.text_input('Job title')
